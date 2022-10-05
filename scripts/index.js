89a3d3981835698fb3203515aced175112df3c71
chris <chrishethertonweb@gmail.com>
2022-10-05 17:00:16 +0200

    added focus() on input button

--- scripts/index.js
+++ scripts/index.js
@@ -8,28 +8,36 @@ console.log("Hello from index.js");
 const inputFilm = document.querySelector('#input')
 const save = document.querySelector('.btn-save')
 const reset = document.querySelector('.btn-reset')
-
+inputFilm.focus()
 const filmArray = []
 const newFilm = new Client()
 const viewOne = new View()
 
+localStorage.setItem('film', newFilm)
+
+
 save.addEventListener('click', () => {
-  const localStorageItem = localStorage('inputFilm')
 
-  const parsed = JSON.parse(localStorageItem)
-
-  filmArray.push(...parsed)
+  // const localStorageItem = localStorage.getItem('inputFilm')
+  // const parsed = JSON.parse(localStorageItem)
+  // filmArray.push(...parsed)
   // if (!localStorageItem.include(inputFilm.value))
 
   newFilm.getMovieData(inputFilm.value)
     .then(data => {
-
       viewOne.displayMovieOnPage(data)
+      filmArray.push(data)
+      localStorage.setItem('inputFilm', JSON.stringify(filmArray))
+
     })
+  // console.log(filmArray);
 
   // filmArray.push(inputFilm.value)
 })
 
 reset.addEventListener('click', () => {
   viewOne.removeDisplay()
+  localStorage.clear()
+  filmArray.length = 0
 })
+console.log(filmArray);
